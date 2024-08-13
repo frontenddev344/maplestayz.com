bce418cb70382ee3ea215016ee46669424e0dd2e
Dev avology <devavology12@gmail.com>
2024-08-13 20:51:40 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,3 +33,30 @@ $(document).ready(function () {
 // Header Scroll JS End
 
 AOS.init();
+
+// //Cookies JS Start
+const cookiesBox = document.querySelector('.wrapper'),
+  buttons = document.querySelectorAll('.button');
+
+// ---- ---- Show ---- ---- //
+const executeCodes = () => {
+  if (document.cookie.includes('AlexGolovanov')) return;
+  cookiesBox.classList.add('show');
+
+  // ---- ---- Button ---- ---- //
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      cookiesBox.classList.remove('show');
+
+      // ---- ---- Time ---- ---- //
+      if (button.id == 'acceptBtn') {
+        document.cookie =
+          'cookieBy= AlexGolovanov; max-age=' + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+
+window.addEventListener('load', executeCodes);
+
+// //Cookies JS Start
